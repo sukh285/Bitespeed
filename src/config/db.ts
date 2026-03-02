78eecf2ce946010a7fc0d1b1f64f72b2b3b8fd5a
Sukhmeet Singh Lamba <130579680+sukh285@users.noreply.github.com>
2026-03-02 18:00:58 +0530

    feat: create custom prisma generate folder for type safety

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,12 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 export const db =
-  globalForPrisma.prisma ?? new PrismaClient({
+  globalForPrisma.prisma ??
+  new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
   });
 
